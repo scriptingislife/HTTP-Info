f3b76f75ac753133243553ed682b0bb40ef93e1e
Ubuntu <ubuntu@proxy.notamalicious.download>
2018-08-18 03:01:31 +0000

    screenshots and logs network traffic

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,20 @@ FROM joyzoursky/python-chromedriver:3.7
 
 RUN pip install selenium==3.8.0
 RUN pip install puush.py
+RUN pip install browsermob-proxy
+
+RUN apt update
+RUN apt install default-jre unzip -y
 
 RUN mkdir /usr/http-info
+WORKDIR /usr/http-info
 
 COPY screenshot.py /usr/http-info
 
-WORKDIR /usr/http-info
+RUN wget https://github.com/lightbody/browsermob-proxy/releases/download/browsermob-proxy-2.1.4/browsermob-proxy-2.1.4-bin.zip
+RUN unzip browsermob-proxy-2.1.4-bin.zip
 
-ENV HTTPINFO_URL "https://www.laperlerare972-service.com/wp-includes/Requests/prosoft/officelite.htm"
+ENV HTTPINFO_BMP_PATH "./browsermob-proxy-2.1.4/bin/browsermob-proxy"
+ENV HTTPINFO_PUUSHAPI "[redacted]"
+ENV HTTPINFO_URL "https://google.com"
 RUN python screenshot.py
--- screenshot.py
+++ screenshot.py
@@ -8,7 +8,9 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from browsermobproxy import Server
+import pprint
 import puush
 import os
 
@@ -16,14 +18,28 @@ ACCOUNT = puush.Account(os.environ['HTTPINFO_PUUSHAPI'])
 DRIVER = 'chromedriver'
 WINDOW_SIZE = "1920,1080"
 
-# Set headless mode
+# Create proxy
+server = Server(os.environ['HTTPINFO_BMP_PATH'])
+server.start()
+proxy = server.create_proxy()
+
+# Set Chrome configuration
 chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--proxy-server={}'.format(proxy.proxy))
+chrome_options.add_argument('--ignore-certificate-errors')
+chrome_options.add_argument('--disable-web-security')
+chrome_options.add_argument('--allow-running-insecure-content')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument("--window-size={}".format(WINDOW_SIZE))
 
-driver = webdriver.Chrome(DRIVER, chrome_options=chrome_options)
+chrome_capabilities = DesiredCapabilities.CHROME.copy()
+chrome_capabilities['acceptSslCerts'] = True
+chrome_capabilities['acceptInsecureCerts'] = True
+
+proxy.new_har('http-info')
+driver = webdriver.Chrome(DRIVER, chrome_options=chrome_options, desired_capabilities=chrome_capabilities)
 
 # Specify URL
 URL = str(os.environ['HTTPINFO_URL'])
@@ -36,5 +52,10 @@ screenshot = driver.save_screenshot(OUT)
 upload = ACCOUNT.upload(OUT)
 print(upload.url)
 
+network_traffic = proxy.har
+server.stop()
+
 driver.quit()
 
+pp = pprint.PrettyPrinter(indent=2)
+pp.pprint(network_traffic)
